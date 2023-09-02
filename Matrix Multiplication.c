093a355cb1246b10664ee0edf175b3aca4c864a6
21CSS101J <113873680+TKAKSHAY2004@users.noreply.github.com>
2023-09-02 17:01:10 +0530

    Create Matrix Multiplication.c

---
+++ Matrix Multiplication.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a[10][10],b[10][10],mul[10][10],r,c,i,j,k;
+    printf("Enter the no.of rows and column : ");
+    scanf("%d %d",&r,&c);
+    
+    printf("enter the elements of first matrix: \n");
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("enter the elements of second matrix: \n");
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++){
+            scanf("%d",&b[i][j]);
+        }
+    }
+    printf("Matrix Multiplication: \n");
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++){
+            mul[i][j]=0;
+            for(k=0;k<c;k++){
+                mul[i][j]+=a[i][k]*b[k][i];
+                
+            }
+        }
+    }
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++){
+            printf("%d\t",mul[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
