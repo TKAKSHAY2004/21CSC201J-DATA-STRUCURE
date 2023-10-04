c83f960b710ca6d4a8c8c67c49ecfd69ee71c72b
21CSS101J <113873680+TKAKSHAY2004@users.noreply.github.com>
2023-10-04 21:51:15 +0530

    Update Matrix Multiplication.c

--- Matrix Multiplication.c
+++ Matrix Multiplication.c
@@ -1,38 +1,55 @@
+/* Author: Akshay Khanna T K
+Date: 25/8/2023
+Aim: Matrix Multiplication */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    int a[10][10],b[10][10],mul[10][10],r,c,i,j,k;
-    printf("Enter the no.of rows and column : ");
-    scanf("%d %d",&r,&c);
-    
-    printf("enter the elements of first matrix: \n");
-    for(i=0;i<r;i++){
-        for(j=0;j<c;j++){
-            scanf("%d",&a[i][j]);
+int main() {
+    int a[100][100], b[100][100], mul[100][100], i, k, j, r1, c1, r2, c2;
+
+    printf("Enter no. of Rows and Columns for Matrix A: ");
+    scanf("%d %d", &r1, &c1);
+
+    printf("Enter no. of Rows and Columns for Matrix B: ");
+    scanf("%d %d", &r2, &c2);
+
+    if (c1 != r2) {
+        printf("Matrix multiplication is not possible. Number of columns in A must be equal to the number of rows in B.\n");
+        return 1;
+    }
+
+    printf("Enter elements of Matrix A:\n");
+    for (i = 0; i < r1; i++) {
+        for (j = 0; j < c1; j++) {
+            scanf("%d", &a[i][j]);
         }
     }
-    printf("enter the elements of second matrix: \n");
-    for(i=0;i<r;i++){
-        for(j=0;j<c;j++){
-            scanf("%d",&b[i][j]);
+
+    printf("Enter elements of Matrix B:\n");
+    for (i = 0; i < r2; i++) {
+        for (j = 0; j < c2; j++) {
+            scanf("%d", &b[i][j]);
         }
     }
-    printf("Matrix Multiplication: \n");
-    for(i=0;i<r;i++){
-        for(j=0;j<c;j++){
-            mul[i][j]=0;
-            for(k=0;k<c;k++){
-                mul[i][j]+=a[i][k]*b[k][i];
-                
+
+    printf("Matrix Multiplication:\n");
+    for (i = 0; i < r1; i++) {
+        for (j = 0; j < c2; j++) {
+            mul[i][j] = 0;
+            for (k = 0; k < c1; k++) {
+                mul[i][j] += a[i][k] * b[k][j];
             }
         }
     }
-    for(i=0;i<r;i++){
-        for(j=0;j<c;j++){
-            printf("%d\t",mul[i][j]);
+
+    for (i = 0; i < r1; i++) {
+        for (j = 0; j < c2; j++) {
+            printf("%d\t", mul[i][j]);
         }
         printf("\n");
     }
+
     return 0;
 }
+
